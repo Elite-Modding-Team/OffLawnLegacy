655ff95bebbcba43fdbab5bcaeeabbdcf287b9cc
Me <135455255+IcarussOne@users.noreply.github.com>
2025-09-10 15:11:18 -0500

    bump version

--- src/main/java/mod/emt/offlawn/OffLawnLegacy.java
+++ src/main/java/mod/emt/offlawn/OffLawnLegacy.java
@@ -27,7 +27,7 @@ public class OffLawnLegacy {
     public static final String MOD_ID = "offlawn";
     public static final String MOD_PREFIX = MOD_ID + ":";
     public static final String NAME = "OffLawn! Legacy";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
     public static final Logger LOGGER = LogManager.getLogger(NAME);
 
